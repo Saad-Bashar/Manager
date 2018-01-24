c856b635449816d554c93dd3ac3c31ad70e9b199
saad <saad@lizard-apps.com>
2018-01-24 17:24:41 +0800

    setup complete

--- App.js
+++ App.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './src/reducers';
 
 export default class App extends React.Component {
+  componentWillMount() {
+    // Initialize Firebase
+    const config = {
+      apiKey: 'AIzaSyBzg5yuCozvwKW4s1N17TWjozVZgTeZT8M',
+      authDomain: 'manager-3ada7.firebaseapp.com',
+      databaseURL: 'https://manager-3ada7.firebaseio.com',
+      projectId: 'manager-3ada7',
+      storageBucket: 'manager-3ada7.appspot.com',
+      messagingSenderId: '688496148637'
+    };
+    firebase.initializeApp(config);
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      /* eslint-disable no-underscore-dangle */
+      /* eslint-disable no-undef */
+      <Provider
+        store={
+        createStore(reducers,
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__())}
+        /* eslint-disable no-underscore-dangle */
+        /* eslint-disable no-undef */
+      >
+        <View style={styles.container}>
+          <Text>I will finish this app!</Text>
+        </View>
+      </Provider>
     );
   }
 }
@@ -16,6 +43,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
 });
