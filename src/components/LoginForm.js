26cd9dcf75aa0fb66645516c74e8f6b9d6a8a997
saad <saad@lizard-apps.com>
2018-01-25 16:38:32 +0800

    password redux state created

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { emailChanged } from '../actions';
+import { emailChanged, passwordChanged } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 class LoginForm extends Component {
  onEmailChange(text) {
    this.props.emailChanged(text);
+ }
+
+ onPasswordChanged(text) {
+   this.props.passwordChanged(text);
  }
 
  render() {
@@ -25,6 +29,7 @@ class LoginForm extends Component {
            secureTextEntry
            label="Password"
            placeholder="password"
+           onChangeText={this.onPasswordChanged.bind(this)}
          />
        </CardSection>
 
@@ -44,4 +49,4 @@ const mapStateToProps = state => {
  };
 };
 
-export default connect(mapStateToProps, { emailChanged })(LoginForm);
+export default connect(mapStateToProps, { emailChanged, passwordChanged })(LoginForm);
